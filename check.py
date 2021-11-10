45ec3f23643c4b2096ba370af72751a32e96ea00
TheBiggerFish <clhnumber4@gmail.com>
2021-11-10 13:16:50 -0500

    Created health-checker with py-cron-schedule

---
+++ check.py
@@ -0,0 +1,103 @@
+"""DOCSTRING"""
+
+import os
+from logging import _nameToLevel
+from typing import Any, Dict
+
+import dotenv
+import requests
+import yaml
+from fishpy.logger import Logger
+from py_cron_schedule import CronFormatError, CronSchedule
+
+dotenv.load_dotenv()
+CONFIG_PATH = os.getenv('CONFIG_PATH')
+LOGGING_HOST = os.getenv('LOGGING_HOST')
+LOGGING_PORT = os.getenv('LOGGING_PORT')
+LOGGING_NAME = os.getenv('LOGGING_NAME')
+LOGGING_LEVEL = os.getenv('LOGGING_LEVEL')
+logger = Logger(LOGGING_NAME,LOGGING_HOST,
+                LOGGING_PORT,LOGGING_LEVEL)
+logger.debug('Logger configured: {{"name":%s,"level":%s,"host":%s,"port":%s}}',
+             LOGGING_HOST,LOGGING_LEVEL,LOGGING_HOST,LOGGING_PORT)
+
+
+def load_config():
+    """Load the service configuration from config.yml file"""
+    try:
+        logger.debug('Opening configuration file: %s', CONFIG_PATH)
+        if CONFIG_PATH is None:
+            logger.fatal('CONFIG_PATH environment variable not defined')
+        with open(CONFIG_PATH,encoding='UTF-8') as config_file:
+            config = yaml.safe_load(config_file)
+    except IOError as err:
+        logger.fatal('Error opening configuration file: %s', str(err))
+    return config
+
+
+def is_online(service, check_data):
+    """Check if a service is online by sending a health check request"""
+
+    logger.debug('Checking status of service {%s}',service)
+    failure_level = _nameToLevel[check_data['failure_log_level']]
+
+    response = requests.get(check_data['url'])
+
+    if 'response' in check_data:
+        if 'expected_code' in check_data['response'] and \
+                response.status_code != check_data['response']['expected_code']:
+            logger.log(failure_level,'Health-check failed for service {%s}, '\
+                'received unexpected status code {%d}',
+                service, response.status_code)
+            return False
+
+        if 'body' in check_data['response'] and \
+                response.text != check_data['response']['body']:
+            logger.log(failure_level,'Health-check failed for service {%s}, '\
+                'received unexpected response body {%d}',
+                service, response.text)
+            return False
+
+        if response.status_code != 200:
+            logger.log(failure_level,'Health-check failed for service {%s}, '\
+                'received unexpected status code {%d}',
+                service, response.status_code)
+            return False
+
+    elif response.status_code != 200:
+        logger.log(failure_level,'Health-check failed for service {%s}, '\
+            'received unexpected status code {%d}',
+            service, response.status_code)
+        return False
+
+    logger.debug('Health-check passed for service {%s}',service)
+    return True
+
+
+def main():
+    """Main function of health check script"""
+
+    config = load_config()
+    scheduler = CronSchedule()
+
+    service_count = len(config['services'])
+    for service in config['services']:
+        cron:str = config['services'][service]['cron']
+        check_data = config['services'][service]['healthcheck']
+        args = (service, check_data)
+
+        try:
+            scheduler.add_task(service, cron, is_online, *args)
+            logger.debug('Service {%s} added to scheduler on schedule "%s" with configuration:\n%s',
+                         service,cron,str(check_data))
+        except CronFormatError:
+            logger.error('Failed to interpet cron "%s" for service {%s}',
+                         cron, service)
+            service_count -= 1
+
+    logger.info('Started health-checker on (%d) services', service_count)
+    scheduler.start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""Docstring"""
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='healthCheck-TheBiggerFish',
+    version='1.0.0',
+    author='TheBiggerFish',
+    author_email='clhnumber4@gmail.com',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/TheBiggerFish/HeathCheck',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'PyYAML',
+        'python-crontab',
+        'python-dotenv',
+        'py-cron-schedule',
+    ],
+    python_requires='>=3.8',
+)
